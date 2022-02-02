0f3c317bf2c259c4612c513e9cde32f6e9c3baf0
Juliana Mozaga Neves <jumozaga@yahoo.com>
2022-02-02 12:29:45 -0300

    Funcionando a logica

--- js/index.js
+++ js/index.js
@@ -7,46 +7,20 @@ const chave = {
     k:'1000&1',l:'1x0011',m:'1000ef',n:'101@01',o:'00110g',
     p:'00001@',q:'1e0101',r:'001001',s:'000101',t:'11100e',
     u:'10e100',v:'110f01',x:'010001',w:'000111',y:'101011',
-    z:'010110'    
+    z:'010110'," ":'000000'   
 }//111#01 10e100 1x0011 0001q1 100@10 101@01 100@10 Juliana
-
-const key = Object.keys(chave).find((key) => chave[key] === '100@10');
-console.log(key); 
-
-
-
-
 
 function descodificar (texto){
   uncode = []
-  const alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','y','z']
-  const num = [0,1,2,3,4,5,6,7,8,9]
   texto.toString().toLowerCase()
-
   for(let i = 0;i<texto.length; i=i+6){    
-    let cifrado = texto.slice(i,i+6)  
-    uncode.push(cifrado)
-    //console.log(cifrado)  
-      for (let j = 0; j<(uncode.length); j++){
-        let al = alpha[j]
-       //console.log(chave.al)
-        let nu = num[j]
-       // console.log(nu)
-       // console.log(cifrado)
-          if(uncode[j] === chave.al){
-            uncode[j] = j
-            //console.log(al)
-          }else if (chave[nu] == cifrado){
-            //console.log(chave[nu])
-          }else{
-            //console.log(chave.al)
-          }
-      }
+    let cifrado = texto.slice(i,i+6)
+    let key = Object.keys(chave).find((key) => chave[key] === cifrado);//retorna a propriedade da cifra(o valor)
+    uncode.push(key)
   }
- //console.log(uncode)   
+  texto = uncode.toString().toUpperCase().replace(/,/g, "").replace(/\./g, "")
+  return console.log(texto)
 }
-descodificar('111#0110e1001x00110001q1100@10101@01100@10')
-
 
 function codificar (texto){
        //Um array para armazenar e trabalhar melhor com os dados
@@ -56,9 +30,11 @@ function codificar (texto){
           let letra = (texto.slice(index,index+1)).toLowerCase() //percorrendo o texto letra por letra (slice(0,1) pega a primeira letra)
           //console.log(letra)
           if (letra == "undefined" || letra == " " || letra == ""){
-              letra = 0;
+              letra = '000000';
+          }else{
+            letra = chave[letra]
           }
-          letra = chave[letra]
+          
           encode.push(letra) //Preenchendo o array com cada letra
                      
     }
@@ -68,7 +44,8 @@ function codificar (texto){
 
 }
     
-//codificar("Juliana");
+codificar("Juliana Mozaga");
+descodificar('111#0110e1001x00110001q1100@10101@01100@100000001000ef00110g010110100@101x0010100@10')
     
     
    
